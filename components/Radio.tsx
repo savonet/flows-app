93cfbc61edddbb7dc6f540c0e7ed122daff5fb52
Romain Beauxis <toots@rastageeks.org>
2022-08-02 21:13:18 -0500

    Basic markup for radio.

--- components/Radio.tsx
+++ components/Radio.tsx
@@ -1,9 +1,23 @@
 import { type Radio } from "@flows/lib/api/radio"
 
 export default function Radio({ radio }: { radio: Radio }) {
+  const displayArtist = radio.artist ? ` by ${radio.artist}` : ""
+  const displayTitle = radio.title ? `${radio.title}${displayArtist}` : ""
+
   return (
-    <div className='rounded shadow-lg h-48 bg-white hover:cursor-pointer border border-gray-200 hover:border-gray-300 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal'>
-      {`Radio: ${radio.name}`}
+    <div className='rounded shadow-lg h-48 bg-white border border-gray-200 hover:border-gray-300 rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal'>
+      <div>{radio.name}</div>
+      <div>{displayTitle}</div>
+      <div>
+        Listen:
+        <ul>
+          {radio.streams.map(({ url, format }, idx) => (
+            <a className='underline' href={url} target='_blank' key={idx} rel="noreferrer">
+              {format}
+            </a>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
